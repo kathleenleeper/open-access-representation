edf69fee76b5ed542422602397286143bc94ea03
joel dapello <joel@biobright.org>
2014-12-19 07:59:54 +0000

    added conversion of 'EN' to 'English'

--- pythonplaytime/parse-oai-pmh.py
+++ pythonplaytime/parse-oai-pmh.py
@@ -9,18 +9,19 @@ for i in range(0,len(f)):
     line = f[i]
     splitLine = str(line).split('>')
     if "title" in splitLine[0]:# if it's a title line
-        title = splitLine[1].split('<')[0] #print the dumb titl      
-    if "date" in splitLine[0]:
-        date = splitLine[1].split('T')[0] #print the dumb titl      
-    if "creator" in splitLine[0]:
-        author.append(splitLine[1].split('<')[0]) #print the dumb titl
+        title = splitLine[1].split('<')[0] # set title variable
+    if "date" in splitLine[0]: # if its the date
+        date = splitLine[1].split('T')[0] # set date variable      
+    if "creator" in splitLine[0]: 
+        author.append(splitLine[1].split('<')[0]) 
     if "language" in splitLine[0]:
-        language = splitLine[1].split('<')[0] #print the dumb title
-        print language
-    if "/record" in splitLine[0]:
-        Article = {"Title": title, "Date": date,"Language": language, "Author": author}
-        author=[]
-        metadata.append(Article)
+        language = splitLine[1].split('<')[0] 
+        if language == "EN": # convert "EN" to English
+            language = "English"
+    if "/record" in splitLine[0]: # if end of record
+        Article = {"Title": title, "Date": date,"Language": language, "Author": author} # define article dictionary
+        metadata.append(Article) # push artical dictionary to metadata array
+        author=[] # reset Author array
 
 print len(metadata)
 print metadata[1]
