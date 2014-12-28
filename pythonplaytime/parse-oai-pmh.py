f823d75818873090dc08e7ec4b59e317e3dc4940
Kathleen Leeper <kathleen.leeper@gmail.com>
2014-12-28 12:46:38 -0500

    clean up/clarify comments

--- pythonplaytime/parse-oai-pmh.py
+++ pythonplaytime/parse-oai-pmh.py
@@ -10,19 +10,22 @@ Article = {}
 for i in range(0,len(f)):
     line = f[i]
     splitLine = str(line).split('>')
-    if "title" in splitLine[0]:# if it's a title line
-        title = splitLine[1].split('<')[0] # set title variable
-    if "date" in splitLine[0]: # if its the date
-        date = splitLine[1].split('T')[0] # set date variable
-    if "description" in splitLine[0]: #  
-        description = splitLine[0].split('<')[0] # set date variable
+    # start pulling data and assigning to variables
+    if "title" in splitLine[0]:
+        title = splitLine[1].split('<')[0] 
+    if "date" in splitLine[0]: 
+        date = splitLine[1].split('T')[0] 
+    if "description" in splitLine[0]:  
+        description = splitLine[0].split('<')[0] 
     if "creator" in splitLine[0]: 
         authors.append(splitLine[1].split('<')[0]) 
     if "language" in splitLine[0]:
         language = splitLine[1].split('<')[0] 
         if language == "EN": # convert "EN" to English
             language = "English"
-    if "/record" in splitLine[0]: # if end of record
+    if "/record" in splitLine[0]: #stop pulling, start writing
+    
+        #pull -first- names from description
         for j in range(0,len(authors)):
             author = authors[j].split(" ")
             for l in description.split(","):
@@ -30,16 +33,16 @@ for i in range(0,len(f)):
                         if len(l.split(" ")) < 7:
                             authors[j] = l.split(" ",1)[1]
         Article = {"Title": title, "Date": date,"Language": language, "Author": authors} # define article dictionary
-        metadata.append(Article) # push artical dictionary to metadata array
+        metadata.append(Article) # push line to metadata
         authors=[] # reset Author array
 
-with open('parsed_oai.csv', 'w') as csvfile: # this could all be embedded into the loop above for efficiency, but was easiest to draft as a second loop.
+# writing article to CSV file (temporary until we figure out how to put R inside python
+with open('parsed_oai.csv', 'w') as csvfile:
     fieldnames = ["Title","Date","Language","Author"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    for i in metadata: # cycles through each artical in metadata array
-        authors = i["Author"] # handle multiple authors per artical
+    for i in metadata: # cycles through articles in metadata array
+        authors = i["Author"] # handle multiple authors per article by making a separate entry
         for j in authors: # cycle through each to make expand csv a la Alex's output_demo.2
             writer.writerow({"Title": i["Title"], "Date": i["Date"],"Language": i["Language"], "Author": j})
 
-# CSV header format Title,Title Alternative,Identifier,Publisher,Language,ISSN,EISSN,Keyword,Start Year,End Year,Added on date,Subjects,Country,Publication fee,Further Information,CC License,Content in DOAJ
