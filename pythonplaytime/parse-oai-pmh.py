fd608203ee9a129cc236f71f20cd9b888be35215
Kathleen Leeper <kathleen.leeper@gmail.com>
2014-12-28 19:37:48 -0500

    make csv writer separate script

--- pythonplaytime/parse-oai-pmh.py
+++ pythonplaytime/parse-oai-pmh.py
@@ -36,13 +36,4 @@ for i in range(0,len(f)):
         metadata.append(Article) # push line to metadata
         authors=[] # reset Author array
 
-# writing article to CSV file (temporary until we figure out how to put R inside python
-with open('parsed_oai.csv', 'w') as csvfile:
-    fieldnames = ["Title","Date","Language","Author"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for i in metadata: # cycles through articles in metadata array
-        authors = i["Author"] # handle multiple authors per article by making a separate entry
-        for j in authors: # cycle through each to make expand csv a la Alex's output_demo.2
-            writer.writerow({"Title": i["Title"], "Date": i["Date"],"Language": i["Language"], "Author": j})
 
