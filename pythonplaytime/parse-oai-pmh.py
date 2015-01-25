f4cb30ff51427277c914cac432b3410fe30c2805
joel dapello <joel@biobright.org>
2015-01-25 02:44:43 +0000

    add call to namsor api

--- pythonplaytime/parse-oai-pmh.py
+++ pythonplaytime/parse-oai-pmh.py
@@ -2,6 +2,8 @@ import codecs
 from gender_detector import GenderDetector #import genderMalev mdule
 import csv
 import re # regular expression searching
+from ast import literal_eval
+from urllib2 import urlopen
 
 from genderComputer import GenderComputer #import gendercomputer - more fully featured
 import os
@@ -13,19 +15,21 @@ gc = GenderComputer(os.path.abspath('./nameLists')) #make gendercomputer
 
 def writeData(metadata): 
     with open('parsed_oai.csv', 'w') as csvfile: # this could all be embedded into the loop above for efficiency, but was easiest to draft as a second loop.
-        fieldnames = ["Title","Date","Language","Author","MGender","CGender","journal","publisher","topics"]
+        fieldnames = ["Title","Date","Language","Author","MGender","CGender","NGender","journal","publisher","topics"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for i in metadata: # cycles through each artical in metadata array
             authors = i["Authors"] # handle multiple authors per artical
             Mgenders = i["Mgenders"]
             Cgenders = i["Cgenders"]
+            Ngenders = i["Ngenders"]
             
             for j in range(0,len(authors)): # cycle through each to make expand csv a la Alex's output_demo.2
                 author = authors[j]
                 Mgender = Mgenders[j]
                 Cgender = Cgenders[j]
-                writer.writerow({"Title": i["Title"], "Date": i["Date"],"Language": i["Language"], "Author": author, "publisher":i["publisher"], "journal":i["source"], "topics":i["subjects"], "MGender": Mgender, "CGender": Cgender})
+                Ngender = Ngenders[j]
+                writer.writerow({"Title": i["Title"], "Date": i["Date"],"Language": i["Language"], "Author": author, "publisher":i["publisher"], "journal":i["source"], "topics":i["subjects"], "MGender": Mgender, "CGender": Cgender, "NGender": Ngender})
 
 metadata = [] 
                 
@@ -37,6 +41,7 @@ def parseData(f):
     Article = {}
     Mgenders = []
     Cgenders = []
+    Ngenders = []
     for i in range(0,len(f)):
         line = f[i]
         splitLine = str(line).split('>')
@@ -72,33 +77,55 @@ def parseData(f):
                     if author[0] in l:
                         digitCheck = re.search("\d", l)  # check for digits
                         asteriskCheck = re.search("\*", l)  # check for asterisks
+                        nameArray = l.split(" ")
                         if digitCheck:
-                            authors[j] = l.split(" ",)[1]
+                            if len(nameArray) > 2:
+                                authors[j] = nameArray[1] +" "+ nameArray[2] # first then last name                     
+                            else:
+                                authors[j] = nameArray[0] +" "+ nameArray[1] # first then last
                         elif asteriskCheck:
-                            authors[j] = l.split(" ")[1]
+                            authors[j] = nameArray[1] +" "+ nameArray[2] # first then last
                         else:
-                            authors[j] = l.strip().split(" ")[0]
+                            stripNameArray = l.strip().split(" ")
+                            authors[j] = stripNameArray[0] +" "+ stripNameArray[1]
             #hacky second-tier cleanup & assigning genders
             for k in range(0,len(authors)):
-                author = authors[k]
-                if re.search("\d",author):
+                print authors
+                authorFirst = authors[k].split(" ")[0]
+                if re.search("\d",authorFirst):
+                    authorFirst = "misparsed"
                     authors[k] = "misparsed"
-                if "&amp" in author:
+                if re.search("&amp",authorFirst):
+                    authorFirst = "misparsed"
                     authors[k] = "misparsed"
-                if "" == author:
+                if "" == authorFirst:
+                    authorFirst = "misparsed"
                     authors[k] = "misparsed"
-                author = authors[k]
-                Mgender = d.guess(author) #guess with malev code
+                Mgender = d.guess(authorFirst) #guess with malev code
                 Mgenders.append(Mgender)
-                Cgender = gc.resolveGender(unicode(author,errors='ignore'), None) #guess with gender computer
+                Cgender = gc.resolveGender(unicode(authorFirst,errors='ignore'), None) #guess with gender computer
                 if Cgender == None:
                     Cgender = "unknown"
                 Cgenders.append(Cgender)
-            Article = {"Title": title, "Date": date,"Language": language, "Authors": authors, "subjects":subjects, "source": source, "publisher":publisher, "Mgenders": Mgenders, "Cgenders": Cgenders} # define article dictionary
+                namsor = "http://api.namsor.com/onomastics/api/json/gendre"
+                authorFirstLast = authors[k].split(" ")
+                if 3 > len(authorFirstLast) > 1: # only works with first and last name
+                    if authorFirstLast[0] != "" and authorFirstLast[1] != "": # filter out arrays from strings like "usa " 
+                        for l in authorFirstLast:
+                            namsor = namsor + "/" + l # construct url
+                        namsorString = urlopen(namsor).read() # make GET request
+                        namsorDict = literal_eval(namsorString) # convert to dict
+                        Ngenders.append(namsorDict['gender']) # append gender result to Ngenders
+                    else:
+                        Ngenders.append("misparsed") # incase of "usa " strings
+                else: 
+                    Ngenders.append("misparsed") # incase of 3<x or x<1
+            Article = {"Title": title, "Date": date,"Language": language, "Authors": authors, "subjects":subjects, "source": source, "publisher":publisher, "Mgenders": Mgenders, "Cgenders": Cgenders, "Ngenders": Ngenders} # define article dictionary
             metadata.append(Article) # push line to metadata
             authors = []
             Mgenders = []
             Cgenders = []
+            Ngenders = []
             subjects = []
     writeData(metadata)
 
