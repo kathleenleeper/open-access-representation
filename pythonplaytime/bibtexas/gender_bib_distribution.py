c9680fc136db382f908f12d16407f29d4e29a27e
Kathleen Leeper <kathleen.leeper@gmail.com>
2015-02-05 00:56:19 -0500

    resolve gender .jpg

--- pythonplaytime/bibtexas/gender_bib_distribution.py
+++ pythonplaytime/bibtexas/gender_bib_distribution.py
@@ -1,16 +1,12 @@
-#import codecs
-#from gender_detector import GenderDetector #import genderMalev mdul
-
 from genderComputer import GenderComputer #import gendercomputer - more fully featured
-#import csv
-#import re # regular expression searching
 import pprint as pp
 import os
-
 import bibtexparser as b #module for bibtexin'
+
 from bibtexparser.bparser import BibTexParser #import to add customization
 from bibtexparser.customization import *
 
+gc = GenderComputer(os.path.abspath('./nameLists')) #make gendercomputer
 bib = 'CriticalOpenNeuro.bib' #bring that bib file in
 
 with open(bib) as bibtex_file:
@@ -18,16 +14,11 @@ with open(bib) as bibtex_file:
     parser.customization = author #parse author fields as individuals
     data = b.load(bibtex_file, parser = parser)
 
-print(data.entries[10])
-
-#for i in  data.entries:
 
 for i in data.entries:
     items  = i.iteritems()
     for k, v in items:
         if k == 'author':
             for j in v:
-                au = ', '.join(reversed(j.split(', ', 1))) #convert from last, first to first, last
-                print au
-               # print j
-        
+                gender = gc.resolveGender(j, None) #resolve gender, yay
+                print j, gender
