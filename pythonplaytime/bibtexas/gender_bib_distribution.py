bc39cfecfbb147fb43d47a511028c986a73f06a4
Kathleen Leeper <kathleen.leeper@gmail.com>
2015-02-17 15:24:06 -0500

    make custom parser customizations for cleaning file

--- pythonplaytime/bibtexas/gender_bib_distribution.py
+++ pythonplaytime/bibtexas/gender_bib_distribution.py
@@ -17,10 +17,23 @@ notav = 0
 auCount = 0
 bib = 'CriticalOpenNeuro.bib' #bring that bib file in
 
+def customizations(record):
+    """Use some functions delivered by the library
+    
+    :param record: a record
+    :returns: -- customized record
+    """
+    record = type(record)
+    record = doi(record)
+    record = convert_to_unicode(record) #clean up for parsing
+    record = author(record) #parse authors into array
+    return record
+
 def loadBibFile():
     with open(bib) as bibtex_file:
         parser = BibTexParser()
-        parser.customization = convert_to_unicode, homogenize_latex_encoding,author #clean up the input and parse authors as individuals
+        parser.homogenize = True
+        parser.customization = customizations
         data = b.load(bibtex_file, parser = parser)
 
 
@@ -46,13 +59,13 @@ for entry in data.entries:
 #'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
 #calculate percents #hacks
 print "authors ungendered:", notav
-#percentMen = men/auCount*100
-#percentWomen = women/auCount*100
-#percentUni = uni/auCount*100
-#percentNotAv = notav/auCount*100
-#print 'author count total:', auCount
-#print 'women:', women,",", "%.2f" % percentWomen, '%'
-#print 'men:', men,",", "%.2f" %  percentMen, '%'
-#print 'unisex:', uni,",", "%.2f" %  percentUni, '%'
-#print 'unassigned:', notav,",", "%.2f" %  percentNotAv, '%'
+percentMen = men/auCount*100
+percentWomen = women/auCount*100
+percentUni = uni/auCount*100
+percentNotAv = notav/auCount*100
+print 'author count total:', auCount
+print 'women:', women,",", "%.2f" % percentWomen, '%'
+print 'men:', men,",", "%.2f" %  percentMen, '%'
+print 'unisex:', uni,",", "%.2f" %  percentUni, '%'
+print 'unassigned:', notav,",", "%.2f" %  percentNotAv, '%'
 
