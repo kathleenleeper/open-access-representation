471304b45b2f00e78ba16c0b09763d93730228de
Kathleen Leeper <kathleen.leeper@gmail.com>
2015-02-17 13:01:43 -0500

    add cleanup customizations to file parsing

--- pythonplaytime/bibtexas/gender_bib_distribution.py
+++ pythonplaytime/bibtexas/gender_bib_distribution.py
@@ -6,6 +6,9 @@ import bibtexparser as b #module for bibtexin'
 from bibtexparser.bparser import BibTexParser #import to add customization
 from bibtexparser.customization import *
 
+""""""""""""""""""""""
+initializing variables 
+"""""""""""""""""""""
 gc = GenderComputer(os.path.abspath('./nameLists')) #make gendercomputer
 women = 0
 men = 0
@@ -14,36 +17,42 @@ notav = 0
 auCount = 0
 bib = 'CriticalOpenNeuro.bib' #bring that bib file in
 
-with open(bib) as bibtex_file:
-    parser = BibTexParser()
-    parser.customization = author #parse author fields as individuals
-    data = b.load(bibtex_file, parser = parser)
+def loadBibFile():
+    with open(bib) as bibtex_file:
+        parser = BibTexParser()
+        parser.customization = convert_to_unicode, homogenize_latex_encoding,author #clean up the input and parse authors as individuals
+        data = b.load(bibtex_file, parser = parser)
 
-for i in data.entries:
-    items  = i.iteritems()
-    for k, v in items:
-        if k == 'author':
-            for j in v:
-                auCount = auCount + 1
-                gender = gc.resolveGender(j, None) #resolve gender, yay
-                if gender == 'male':
-                    men = men + 1
-                elif gender == 'female':
-                    women = women + 1
-                elif gender == 'unisex':
-                    uni = uni + 1
-                else: 
-                    notav = notav + 1
+
+for entry in data.entries:
+    title = entry["title"]
+    if "author" in entry:
+        authors = entry["author"]
+    else:
+        print "no author in", title
+    for j in authors:
+        auCount = auCount + 1
+        gender = gc.resolveGender(j, None) #resolve gender, yay
+        if gender == 'male':
+            men += 1
+        elif gender == 'female':
+            women += 1
+        elif gender == 'unisex':
+            uni += 1
+        else: 
+            #print j, title
+            notav += 1 
+
 #'The value of x is ' + repr(x) + ', and y is ' + repr(y) + '...'
 #calculate percents #hacks
-
-percentMen = men/auCount*100
-percentWomen = women/auCount*100
-percentUni = uni/auCount*100
-percentNotAv = notav/auCount*100
-print 'author count total:', auCount
-print 'women:', women,",", "%.2f" % percentWomen, '%'
-print 'men:', men,",", "%.2f" %  percentMen, '%'
-print 'unisex:', uni,",", "%.2f" %  percentUni, '%'
-print 'unassigned:', notav,",", "%.2f" %  percentNotAv, '%'
+print "authors ungendered:", notav
+#percentMen = men/auCount*100
+#percentWomen = women/auCount*100
+#percentUni = uni/auCount*100
+#percentNotAv = notav/auCount*100
+#print 'author count total:', auCount
+#print 'women:', women,",", "%.2f" % percentWomen, '%'
+#print 'men:', men,",", "%.2f" %  percentMen, '%'
+#print 'unisex:', uni,",", "%.2f" %  percentUni, '%'
+#print 'unassigned:', notav,",", "%.2f" %  percentNotAv, '%'
 
